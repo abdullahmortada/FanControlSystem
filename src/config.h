7536363fdd30c2fe7da9d47e0642f619d9325932
AhmedSamy2004 <104323652+AhmedSamy2004@users.noreply.github.com>
2023-12-03 17:38:54 +0200

    added the 3 button and the temperature reading, added 4 modes that you can switch from and the custom mode that you can change the speed

--- src/config.h
+++ src/config.h
@@ -6,7 +6,16 @@
 #define F_CPU 16000000UL
 #define BAUD 9600 
 
-#define ADC_VOLT_PER_STEP 5.0/1024.0 
+#define ADC_VOLT_PER_STEP 5000/1024.0 
+
+#define button1 dio_SetDirection('b', 0, INPUT_PULLUP) && dio_GetBit('b',0)
+
+
+#define button2   dio_SetDirection('b', 1, INPUT_PULLUP) && dio_getbit('b',1)
+
+
+#define button3  dio_SetDirection('b', 2, INPUT_PULLUP) && dio_getbit('b',2)
+
 
 //LCD config
 #define LCD_Dir  DDRD	
--- src/main.c
+++ src/main.c
@@ -3,9 +3,66 @@
 #include "lcd.h"
 #include "dio.h"
 #include "config.h"
+#include "eeprom.h"
 
 
 int main() {
+    
+    uart_Init(BAUD);
+    Adc_int();
+    lcd_int();
+
+    pwm_Init(PWM_PD6);
+    
+    int i=0 ;
+    int speed=0
+
+    mode[i]={cooling,extreme cooling, heating ,custom}
+
+    while(1)
+    {
+
+        int analog=adc_ReadChannel(1);
+        int heat=analog*ADC_VOLT_PER_STEP 
+;
+        //5000 v cause milli volt
+        //n number of bits
+        int temp= heat/10;
+        //make it degree celcius 10 mv per 1 degree
+
+
+
+        if (button1 != 0)
+        {
+            mode[i++];
+            else if (i<5)
+
+            {
+            i=0;
+            }
+            
+
+        }
+        if (i==4  && button2 !=0)
+        {
+            //custom mode 
+            speed+=15;
+            pwm_DutyCycle(PWM_PD6, speed);
+            
+            
+            
+        }
+         if (i==4  && button3 != 0)
+        {
+            //custom mode 
+            speed-=15;
+            pwm_DutyCycle(PWM_PD6, speed);
+            
+            
+        }
+    }
+   
+
 }
 
 
